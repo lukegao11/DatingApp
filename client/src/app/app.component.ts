8a9a59f6cbd347e4f982fabab6d28a0853cf181f
lukegao11 <guanqun.gao@marylandsmith.umd.edu>
2023-06-05 16:44:31 -0400

    section5-halfway

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { JsonPipe } from '@angular/common';
+import { User } from './_model/user';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +12,14 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'Dating App';
   users:any;
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private accountSerivce: AccountService){}
+  
   ngOnInit(): void {
+    this.getUser();
+    this.setCurrentUser();
+  }
+
+  getUser(){
     this.http.get('https://localhost:5001/api/users').subscribe(
       {
         next: response => this.users = response,
@@ -19,6 +28,15 @@ export class AppComponent implements OnInit{
       }
     )
   }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountSerivce.setCurrentUser(user);
+  }
+
+
 
   
 }
