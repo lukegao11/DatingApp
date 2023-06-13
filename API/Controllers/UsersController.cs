bbc43512f1226a2f7ab51257ef63de3689edf081
lukegao11 <guanqun.gao@marylandsmith.umd.edu>
2023-06-13 16:37:56 -0400

    section8-halfway

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-
+    [Authorize]
     public class UsersController : BaseAPiController
     {
         private readonly DataContext _context;
@@ -23,7 +23,7 @@ namespace API.Controllers
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
+        
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
