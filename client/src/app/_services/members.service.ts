46d3e2a676477fbf51f27dd9287455e835f387b6
lukegao11 <guanqun.gao@marylandsmith.umd.edu>
2023-06-16 16:00:43 -0400

    section9-near-end

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -11,24 +11,25 @@ export class MembersService {
   constructor(private http: HttpClient) { }
   
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + '/users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + '/users');
   }
   
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + '/users/' + username, this.getHttpOptions());
+    return this.http.get<Member>(this.baseUrl + '/users/' + username);
   }
 
-  //set http option for token
-  getHttpOptions(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    return{
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
-  }
+  // no longer needed as we have created interceptor for authorization (section9)
+  // //set http option for token
+  // getHttpOptions(){
+  //   const userString = localStorage.getItem('user');
+  //   if(!userString) return;
+  //   const user = JSON.parse(userString);
+  //   return{
+  //     headers: new HttpHeaders({
+  //       Authorization: 'Bearer ' + user.token
+  //     })
+  //   }
+  // }
 
 
 }
